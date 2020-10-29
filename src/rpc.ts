5c013054235efd7dc85f27708555581b5c805cad
David Mihal <david@dmihal.com>
2020-10-29 14:53:10 +0000

    try eth_signTypedData_v4, fall back to eth_signTypedData

--- src/rpc.ts
+++ src/rpc.ts
@@ -33,8 +33,16 @@ export interface RSV {
 }
 
 export const signData = async (provider: any, fromAddress: string, typeData: any): Promise<RSV> => {
-  // const _typeData = typeof typeData === 'string' ? typeData : JSON.stringify(typeData);
-  const result = await send(provider, 'eth_signTypedData', [fromAddress, typeData]);
+  const typeDataString = typeof typeData === 'string' ? typeData : JSON.stringify(typeData);
+  const result = await send(provider, 'eth_signTypedData_v4', [fromAddress, typeDataString])
+    .catch((error: any) => {
+      if (error.message === 'Method eth_signTypedData_v4 not supported.') {
+        return send(provider, 'eth_signTypedData', [fromAddress, typeData]);
+      } else {
+        throw error;
+      }
+    });
+
   return {
     r: result.slice(0, 66),
     s: '0x' + result.slice(66, 130),
